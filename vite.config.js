b33f256f2d98535e4ce02ec6199c5c2e9f24ab0a
Abouerrahim Riahi <atlasshopy@gmail.com>
2025-08-04 22:43:24 +0200

    fix base paths for gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/livewebmoon/' : '/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/livewebmoon/' : '/',
   plugins: [react()],
-})
+}))
